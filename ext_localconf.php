1c1a950885d4f2ef2ac8cc95154bc0c594c5ac05
Steffen Mächtel <s.maechtel@netzbewegung.com>
2024-02-13 18:51:50 +0100

    Compatibility with TYPO3 12

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 /**
  * @author Steffen Maechtel <info@steffen-maechtel.de>
  */
-defined('TYPO3_MODE') || die('Access denied.');
+defined('TYPO3') || die('Access denied.');
 
 call_user_func(function () {
     $isAlreadyExtended = call_user_func(function ($className) {
